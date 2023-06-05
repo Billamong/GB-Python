f77ecfad5a38faccaa01b6d2d74cc505304072d2
Billamong <Dnik1993@yandex.ru>
2023-06-05 16:22:49 +0300

    Zad 30

--- zad 24.py
+++ zad 24.py
@@ -1,25 +1,38 @@
-import random
+# Черника
 
-kust = int(input("введите количество кустов: "))
-array = []
-result = 0
-i = 0
-sum = 0
-maxsum = 0
-for i in range(kust):
-    array.append(int(input(f"Введите количество ягод на {i+1} кусту : ")))
-print(array)
+# kust = int(input("введите количество кустов: "))
+# array = []
+# result = 0
+# i = 0
+# sum = 0
+# maxsum = 0
+# for i in range(kust):
+#     array.append(int(input(f"Введите количество ягод на {i+1} кусту : ")))
+# print(array)
 
-while (i < kust):
-    if (i == 0):
-        sum = array[i] + array[kust - 1] + array[i + 1]
-    elif ( i == kust -1 ):
-        sum = array[0] + array[i] + array[i - 1]
-    else:
-        sum = array[i] + array[i - 1] + array[i + 1]
+# while (i < kust):
+#     if (i == 0):
+#         sum = array[i] + array[kust - 1] + array[i + 1]
+#     elif ( i == kust -1 ):
+#         sum = array[0] + array[i] + array[i - 1]
+#     else:
+#         sum = array[i] + array[i - 1] + array[i + 1]
 
-    if maxsum < sum :
-        maxsum = sum
-    i += 1
+#     if maxsum < sum :
+#         maxsum = sum
+#     i += 1
 
-print(f"максимальное число ягод за одну итерацию {maxsum} ")
+# print(f"максимальное число ягод за одну итерацию {maxsum} ")
+
+n = int(input())
+array = [int(i) for i in input().split()][:n]
+max_summa = 0
+for i in range(1, len(array) - 1):
+   if max_summa < array[i - 1] + array[i] + array[i + 1]:
+      max_summa = array[i - 1] + array[i] + array[i + 1]
+
+if max_summa < array[0] + array[1] + array[len(array) - 1]:
+   max_summa = array[0] + array[1] + array[len(array) - 1]
+if max_summa < array[0] + array[len(array) - 1] + array[len(array) - 2]:
+   max_summa = array[0] + array[len(array) - 1] + array[len(array) - 2]
+print(max_summa)
--- zad 28.py
+++ zad 28.py
@@ -5,7 +5,7 @@ def sum(a,b):
     return sum(a -1, b+1)
 
 
-a = int(input("Введите число a"))
-b = int(input("Введите число b"))
+a = int(input("Введите число a "))
+b = int(input("Введите число b "))
 
 print (sum(a,b))
